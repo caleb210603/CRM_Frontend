91ad7426659b9299371f7f11ede69865a2d218f4
Jefferson <jefferson.silvaquinto@gmail.com>
2024-07-11 20:54:02 -0500

    Refactorizando context notificaciones

--- src/contexts/Notification.tsx
+++ src/contexts/Notification.tsx
@@ -1,108 +1,68 @@
+import React, { ReactNode, createContext, useContext, useState } from "react";
 import { Notification, NotificationId, ResponseCreateNotification } from "@/types/notification";
-import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
+import useWebSocket from "@/hooks/useWebSocket";
 
+// Define la interfaz del estado del proveedor de notificaciones
 interface NotificationsProviderState {
-    notifications: Notification[],
-    setNotifications: Dispatch<SetStateAction<Notification[]>>,    
-    deleteNotification: (message: NotificationId)=>void,
-    createNotification: (message: ResponseCreateNotification)=>void,
-    getListNotifications: (page: number) =>void,
-    loading: boolean
+    notifications: Notification[];
+    setNotifications: React.Dispatch<React.SetStateAction<Notification[]>>;
+    deleteNotification: (id: NotificationId) => void;
+    createNotification: (notification: ResponseCreateNotification) => void;
+    getListNotifications: (page: number) => void;
+    loading: boolean;
+    archiveNotification: (userId: number, notificationId: number) => void;
 }
 
+// Crea el contexto de notificaciones con valores predeterminados
 export const NotificationContext = createContext<NotificationsProviderState>({
     notifications: [],
     setNotifications: () => {},
-    deleteNotification: ()=>{},
-    createNotification: ()=>{},
-    getListNotifications: ()=>{},
-    loading: false
+    deleteNotification: () => {},
+    createNotification: () => {},
+    getListNotifications: () => {},
+    loading: false,
+    archiveNotification: () => {}
 });
 
-const NotificationProvider = ({children}: {children: ReactNode}) => {
+// Proveedor de notificaciones
+const NotificationProvider = ({ children }: { children: ReactNode }) => {
     const [notifications, setNotifications] = useState<Notification[]>([]);
-    const [socket, setSocket] = useState<WebSocket | null>(null)
     const [hasNext, setHasNext] = useState(false);
-    const [loading, setLoading] = useState(false);
-    
-    useEffect(()=>{
-        const socketUrl = 'ws://127.0.0.1:8000/ws/notifications/';
-    
-        const websocket = new WebSocket(socketUrl);
 
-        websocket.onopen = ()=>{
-            console.log('connected')
-        }
-    
-        websocket.onmessage = (event)=>{
-            const response = JSON.parse(event.data)            
-
-            if(response.type == 'single-list') {      
-                const notification = response.data;
-                const notification_transformed = {...notification, date: new Date(notification.date)}                
-                setNotifications(prevNotifications => [notification_transformed, ...prevNotifications])
-            }else if(response.type == 'list-all') {
-                const has_next = response.has_next;
-                const notifications = response.notifications as Notification[];
-                const notifications_transformed = notifications.map((nt)=>{
-                    return {...nt, date: new Date(nt.date)}
-                })
-                setNotifications(prevNotifications => [...prevNotifications, ...notifications_transformed])
-                setHasNext(has_next);
-            }                       
-
-            setLoading(false);
-        }        
-
-        websocket.onclose = () => {
-            console.log('WebSocket disconnected');
-        }
-        
-        // Manejar eventos de error
-        websocket.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
-
-        // Guardar el socket en el estado
-        setSocket(websocket);        
-
-        // Cerrar el socket cuando el componente se desmonte
-        return () => {
-            websocket.close();
-        };
-    }, []);
-
-    const createNotification = (message: ResponseCreateNotification) => {                
-        socket?.send(JSON.stringify({
-            action: 'create',
-            ...message
-        }))                
+    const onNotificationReceived = (notification: Notification) => {
+        setNotifications(prevNotifications => [notification, ...prevNotifications]);
     };
 
-    const deleteNotification = (id: NotificationId)=>{
-        setLoading(true);
-        socket?.send(JSON.stringify({
-            action: 'delete',
-            id
-        }))
-    }
-    
-    const getListNotifications = (page: number)=>{
-        if(!hasNext) return;
-        setLoading(true);
-        socket?.send(JSON.stringify({
-            action: 'list',
-            page
-        }))
-    }
+    const onNotificationsListReceived = (notifications: Notification[], hasNext: boolean) => {
+        setNotifications(prevNotifications => [...prevNotifications, ...notifications]);
+        setHasNext(hasNext);
+    };
+
+    const {
+        createNotification,
+        deleteNotification,
+        archiveNotification,
+        getListNotifications,
+        loading,
+    } = useWebSocket({ onNotificationReceived, onNotificationsListReceived, hasNext });
 
     return (
-        <NotificationContext.Provider value={{notifications, loading, getListNotifications, setNotifications, createNotification, deleteNotification}}>
+        <NotificationContext.Provider
+            value={{
+                notifications,
+                setNotifications,
+                deleteNotification,
+                createNotification,
+                getListNotifications,
+                loading,
+                archiveNotification,
+            }}
+        >
             {children}
         </NotificationContext.Provider>
     );
 };
 
-export const useNotification = ()=> useContext(NotificationContext)
+export const useNotification = () => useContext(NotificationContext);
 
 export default NotificationProvider;
---
+++ src/hooks/useWebSocket.ts
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import { Notification, NotificationId, ResponseCreateNotification } from "@/types/notification";
+import { id } from "date-fns/locale";
+
+interface UseWebSocketProps {
+    onNotificationReceived: (notification: Notification) => void;
+    onNotificationsListReceived: (notifications: Notification[], hasNext: boolean) => void;
+    hasNext: boolean;
+}
+
+const useWebSocket = ({ onNotificationReceived, hasNext, onNotificationsListReceived }: UseWebSocketProps) => {
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const socketUrl = 'ws://127.0.0.1:8000/ws/notifications/';
+        const websocket = new WebSocket(socketUrl);
+
+        websocket.onopen = () => {
+            console.log('Connected to WebSocket');
+        };
+
+        websocket.onmessage = (event) => {
+            const response = JSON.parse(event.data);
+            if (response.type === 'single-list') {
+                const notification = { ...response.data, date: new Date(response.data.date) };
+                onNotificationReceived(notification);
+            } else if (response.type === 'list-all') {
+                const notifications = response.notifications.map((notification: Notification) => ({
+                    ...notification,
+                    date: new Date(notification.date),
+                }));
+                onNotificationsListReceived(notifications, response.has_next);
+            }
+            setLoading(false);
+        };
+
+        websocket.onclose = () => {
+            console.log('WebSocket disconnected');
+        };
+
+        websocket.onerror = (error) => {
+            console.error('WebSocket error:', error);
+        };
+
+        setSocket(websocket);
+
+        return () => {
+            websocket.close();
+        };
+    }, []);
+
+    const createNotification = (notification: ResponseCreateNotification) => {
+        if (socket) {
+            socket.send(JSON.stringify({ action: 'create', ...notification }));
+        }
+    };
+
+    const deleteNotification = (id: NotificationId) => {
+        if (socket) {
+            setLoading(true);
+            socket.send(JSON.stringify({ action: 'delete', id }));
+        }
+    };
+
+    const archiveNotification = (userId: number, notificationId: number) => {
+        if (socket) {
+            socket.send(JSON.stringify({ action: 'archive', notification_id: notificationId, user_id: userId }));
+        }
+    };
+
+    const getListNotifications = (page: number) => {
+        if (socket) {
+            if(!hasNext) return
+            setLoading(true);
+            socket.send(JSON.stringify({ action: 'list', page }));
+        }
+    };
+
+    return {
+        createNotification,
+        deleteNotification,
+        archiveNotification,
+        getListNotifications,
+        loading,
+    };
+};
+
+export default useWebSocket;
